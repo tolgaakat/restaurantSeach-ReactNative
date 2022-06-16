0aeac38ae2beb7d83fd8b233fa6c3f6176e8ea71
tolgaakat <40713567+tolgaakat@users.noreply.github.com>
2022-06-16 09:50:43 +0300

    password masked

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -5,7 +5,7 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://api.yelp.com/v3/businesses",
     headers: {
-        Authorization: 'Bearer 9kOLmWjMV-166hINlE08GMJJGHRvVZ1OSdNtErAhTfkPiAekh-COc-OMqAzE_zBmIcNMPWAXVKx56KlNuFi6geJqDLE-vdN5mbcHc_barj6yPZrd6nByUkXT5lWoYnYx'
+        Authorization: 'Bearer #################'
     }
 });
 
